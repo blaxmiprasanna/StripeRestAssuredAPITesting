9f976ad5747a38d385cbfbb15fab47b6e59d3a62
blaxmiprasanna <blaxmiprasanna@yahoo.com>
2022-05-18 21:03:17 -0400

    updated the ITestListeners java class

--- src/test/java/extendedReport/ExtentListeners.java
+++ src/test/java/extendedReport/ExtentListeners.java
@@ -86,30 +86,4 @@ public class ExtentListeners implements ITestListener {
 
 	}
 	
-	@Test
-	public void postRequestUsingFile() throws Exception{
-		
-		try {
-//			testReport.get().log(Status.INFO, "post request using payload from file is started");
-			String file = HelperClass.readDataFromPropertyFile().getJsonFile();
-			Response response = PayloadToCreatePostRequestWithFile.payloadToCreatePostRequestWithFile(
-					HelperClass.readDataFromPropertyFile().getKey(),
-					HelperClass.readDataFromPropertyFile().getContentType(), file,
-					HelperClass.readDataFromPropertyFile().getBaseURL(),
-					HelperClass.readDataFromPropertyFile().getBasePathForPost());
-//			System.out.println(result.getTestClass().getName());
-//			System.out.println(result.getMethod().getMethodName());
-			System.out.println(response.jsonPath().get("place_id"));
-			testReport.get().log(Status.PASS, "place is generated");
-//Testing report script is not working 
-			//Testing the webhook
-			
-		} catch (Exception e) {
-			System.out.println("Hello");
-			System.out.println(e.getMessage());
-			System.out.println(e.getCause());
-			System.out.println(e.getStackTrace().toString());
-		}
-	}
-
 }
